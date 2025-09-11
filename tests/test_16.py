Semi-unstable roll sign fix

tests/test_16.py
@@ -55,9 +55,9 @@ positions_log = []
 output_file = "positions_log.json"
 
 C = np.array([
-    [1.0, 0.0, 0.0],
+    [-1.0, 0.0, 0.0],
     [0.0, 0.0, 1.0],
-    [0.0, -1.0, 0.0]
+    [0.0, 1.0, 0.0]
 ])
 angles_thresh = 2
 prev_q = None
@@ -95,14 +95,13 @@ try:
             # Solve IK for joint angles (in radians)
             q_sol = kin.ik(move.get_q_guess(), T, frame=frame, max_iters=10, weights6=move.default_weights6)
             q_sol = np.degrees(q_sol)
+            q_sol[4] -= 90
             
             # Clip q_sol values to prevent erratic behaviour
             if prev_q is not None:
                 q_sol = np.clip(q_sol, prev_q - angles_thresh, prev_q + angles_thresh)
 
             prev_q = q_sol
-            print(q_sol)
-            #q_sol[4] = 90 - q_sol[4]
             q_sol = np.append(q_sol, 90 - controls["slider"] * 90)
             action_dict = move._create_action_dict(q_sol)
             move.robot.send_action(action_dict)