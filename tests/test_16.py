38281b56bea1c43f9abb0f46ad6da8a6ea217acf
Giovanni Pegoraro <giovipegoraro@gmail.com>
2025-09-11 19:16:29 +0200

    Added controls support

--- tests/test_16.py
+++ tests/test_16.py
@@ -46,88 +46,57 @@ frame = viz.link_names[5]  # 6th link is end-effector
 
 move.home()
 
-start_position, start_quaternion, _ = receiver.receive()
-start_position = remap_convert(start_position)
-current_pos = move.compute_ee_pos(frame=frame)
+controller_zero, _, _ = receiver.receive()
+controller_zero = remap_convert(controller_zero)
+start_position = np.array(move.compute_ee_pos(frame=frame))
 
 position = None
 positions_log = []
 output_file = "positions_log.json"
 
+C = np.array([
+    [1.0, 0.0, 0.0],
+    [0.0, 0.0, 1.0],
+    [0.0, -1.0, 0.0]
+])
+
 try:
-    i = 0
     while True:
         position, quaternion, controls = receiver.receive()
-        print(quaternion)
-        position = remap_convert(position)
 
-        target_pos = position - start_position + np.array(current_pos)
-        positions_log.append(target_pos.tolist())
+        if controls["button2"] is True:
+            move.home()
 
-        save_positions_to_file(positions_log, output_file)
-        
-        # Remap Unity rotation to robot frame: R_robot = C * R_unity * C^T
-        
-        C = np.array([
-            [1.0, 0.0, 0.0],
-            [0.0, 0.0, 1.0],
-            [0.0, -1.0, 0.0]
-        ])
-        
-        D = np.array([
-            [0.0, 1.0, 0.0],
-            [0.0, 0.0, 1.0],
-            [1.0, 0.0, 0.0]
-        ])
+            while controls["button2"] is True:
+                position, quaternion, controls = receiver.receive()
 
-        # --- Method A: Matrix route ---
-        R_unity = R.from_quat(quaternion).as_matrix()
-        R_robot = C @ R_unity @ C.T
-        q_robot = R.from_matrix(R_robot).as_quat()
+            controller_zero = remap_convert(position)
+            start_position = np.array(move.compute_ee_pos(frame=frame))
+        elif controls["button1"] is not True:
+            position = remap_convert(position)
 
-        rr = D @ R_robot @ D.T
+            target_pos = position - controller_zero + start_position
+            positions_log.append(target_pos.tolist())
+            save_positions_to_file(positions_log, output_file)
+            
+            # Remap Unity rotation to robot frame
+            R_unity = R.from_quat(quaternion).as_matrix()
+            R_robot = C @ R_unity @ C.T
 
-        print("Remapped quaternion (matrix method):", q_robot)
+            # Build target SE(3) pose
+            T = np.eye(4)
+            T[:3, :3] = R_robot
+            T[:3, 3] = target_pos
 
-
-
-
-
-
-
-        # Build base rotation matrix
-        target_horizontal = np.array([target_pos[0], target_pos[1], 0.0])
-        x_axis = target_horizontal / np.linalg.norm(target_horizontal)
-        z_axis = np.array([0, 0, 1])
-        y_axis = np.cross(z_axis, x_axis)
-        rot_base = np.column_stack([x_axis, y_axis, z_axis])
-
-        # Add end-effector rotations (in degrees)
-        tilt_angle = 90   # rotation about local Y (pitch)
-        roll_angle = -90   # rotation about local X (roll)
-        tilt_rot = R.from_euler('y', tilt_angle, degrees=True).as_matrix()
-        roll_rot = R.from_euler('x', roll_angle, degrees=True).as_matrix()
-
-        # Apply rotations to base rotation (order: tilt then roll in EE frame)
-        rot_to_target = rot_base @ tilt_rot @ roll_rot
-
-        R_ee = R.from_quat(quaternion).as_matrix()
-        rot_to_target = rot_base @ R_robot
-
-        # Build target SE(3) pose
-        T = np.eye(4)
-        T[:3, :3] = R_robot
-        T[:3, 3] = target_pos
-
-        # Solve IK for joint angles (in radians)
-        q_sol = kin.ik(move.get_q_guess(), T, frame=frame, max_iters=10, weights6=move.default_weights6)
-        current_q = q_sol.copy()  # Update current joint state with best effort
-        q_sol = q_sol
-
-
-        q_sol = np.degrees(q_sol)
-        action_dict = move._create_action_dict(q_sol)
-        move.robot.send_action(action_dict)
+            # Solve IK for joint angles (in radians)
+            q_sol = kin.ik(move.get_q_guess(), T, frame=frame, max_iters=10, weights6=move.default_weights6)
+            q_sol = np.degrees(q_sol)
+            q_sol = np.append(q_sol, 90 - controls["slider"] * 90)
+            action_dict = move._create_action_dict(q_sol)
+            move.robot.send_action(action_dict)
+        else:
+            controller_zero = remap_convert(position)
+            start_position = np.array(move.compute_ee_pos(frame=frame))
 
 except KeyboardInterrupt:
     save_positions_to_file(positions_log, output_file)
